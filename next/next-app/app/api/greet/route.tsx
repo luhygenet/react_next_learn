2a12a3c7de83fbf38968dbe42397cecd6c501ba9
Liya Daniel <luhygenet@gmail.com>
2025-08-28 00:37:04 +0300

    next's extra things like nextobject and its searchParams

--- next/next-app/app/api/greet/route.tsx
+++ next/next-app/app/api/greet/route.tsx
@@ -2,9 +2,17 @@
 
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
-  // Example: /api/greet?name=Liya
-  const name = request.nextUrl.searchParams.get("name") || "Stranger";
+// export async function GET(request: NextRequest) {
+//   // Example: /api/greet?name=Liya
+//   const name = request.nextUrl.searchParams.get("name") || "Stranger";
 
-  return Response.json({ message: `Hello, ${name}!` });
+//   return Response.json({ message: `Hello, ${name}!` });
+// }
+
+// example 2
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('query')
+  // query is "hello" for /api/search?query=hello
+  return Response.json({ message: `Hello, ${query}!` });
 }
