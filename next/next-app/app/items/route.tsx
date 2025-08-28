80bb40cad5873d3d66ad6a9b6e645e9889ec9be4
Liya Daniel <luhygenet@gmail.com>
2025-08-29 00:37:17 +0300

    added json version of request object accepting

--- next/next-app/app/items/route.tsx
+++ next/next-app/app/items/route.tsx
@@ -1,12 +1,28 @@
+// // app/items/route.ts
+
+//formdata accepting version
+// export async function POST(request: Request) {
+//   // Expecting formData body
+//   const formData = await request.formData()
+//   const name = formData.get("name")
+//   const email = formData.get("email")
+
+//   return Response.json({
+//     message: "Received FormData",
+//     name,
+//     email,
+//   })
+// }
+
 // app/items/route.ts
+//json accepting version
 export async function POST(request: Request) {
   // Expecting formData body
-  const formData = await request.formData()
-  const name = formData.get("name")
-  const email = formData.get("email")
+  const body = await request.json()
+  const {name,email} = body
 
   return Response.json({
-    message: "Received FormData",
+    message: "Received FormDta",
     name,
     email,
   })
